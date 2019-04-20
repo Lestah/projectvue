c6022ad56c5ce32f67879c9937980e446d330098
Lestah <sjelchan25@yahoo.com>
2019-04-20 16:58:38 +0800

    Design answers table schema

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -23,7 +23,7 @@
                                     <strong>{{ $question->votes }}</strong> {{ str_plural('vote', $question->votes) }}
                                 </div>
                                 <div class="status {{ $question->status }}">
-                                    <strong>{{ $question->answers }}</strong> {{ str_plural('answer', $question->votes) }}
+                                    <strong>{{ $question->answers_count }}</strong> {{ str_plural('answer', $question->answers_count) }}
                                 </div>
                                 <div class="view">
                                     {{ $question->views . " " . str_plural('view', $question->views) }}
@@ -33,12 +33,16 @@
                                 <div class="d-flex align-items-center">
                                 <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
                                     <div class="ml-auto">
+                                        @can ('update', $question)
                                         <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
+                                        @endcan
+                                        @can ('delete', $question)
                                         <form class="form-delete" method="post" action="{{ route('questions.destroy', $question->id) }}">
                                             @method('DELETE')
                                             @csrf
                                             <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are your sure')">Delete</button>
                                         </form>
+                                        @endcan
                                     </div>
                                 </div>
                                
